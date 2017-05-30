52fa745a80e8cc98e49cc25684a62d8016c39fea
nshkuratova <nika.shkuratova@gmail.com>
2017-05-30 17:28:50 +0300

    TestNG added to Maven dependencies

--- src/test/java/com/epam/nika_shkuratava/SumCalculatorTest.java
+++ src/test/java/com/epam/nika_shkuratava/SumCalculatorTest.java
@@ -1,7 +1,44 @@
 package com.epam.nika_shkuratava;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 /**
  * Created by Nika_Shkuratava on 5/30/2017.
  */
 public class SumCalculatorTest extends BaseCalculatorTest {
+
+    @DataProvider(name = "calculatorSumDoubleDataProvider")
+    public Object[][] sumDoubleDataProvider() {
+        return new Object[][]{
+                {1.4, 1.8, 3.2},
+                {0.0, 0.0, 0.0},
+                {2.0, 3.0, 5.0}
+
+        };
+    }
+
+    @DataProvider(name = "calculatorSumLongDataProvider")
+    public Object[][] sumLongDataProvider() {
+        return new Object[][]{
+                {4, 8, 12},
+                {0, 0, 0},
+                {2, 3, 5}
+
+        };
+    }
+
+    @Test(dataProvider = "calculatorSumDoubleDataProvider")
+    public void checkDoubleSum(double a, double b, double expectedResult) {
+        double result = calculator.sum(a, b);
+        Assert.assertEquals(result, expectedResult);
+    }
+
+
+    @Test(dataProvider = "calculatorSumLongDataProvider")
+    public void checkLongSum(long a, long b, long expectedResult) {
+        long result = calculator.sum(a, b);
+        Assert.assertEquals(result, expectedResult);
+    }
 }
