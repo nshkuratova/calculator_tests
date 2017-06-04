e9165412ea6c7c6dfaa0ad5504593a032678a4dc
Nika Shkuratava <nika_shkuratava@epam.com>
2017-06-04 10:44:26 +0300

    tests added

--- src/test/java/com/epam/nika_shkuratava/BaseCalculatorTestTestNG.java
+++ src/test/java/com/epam/nika_shkuratava/BaseCalculatorTestTestNG.java
@@ -1,7 +1,7 @@
 package com.epam.nika_shkuratava;
 
 import com.epam.tat.module4.Calculator;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
 
 /**
  * Created by Nika_Shkuratava on 5/30/2017.
@@ -10,9 +10,34 @@ public abstract class BaseCalculatorTestTestNG {
 
     protected Calculator calculator;
 
+    @BeforeGroups
+    public void beforeGroups() {
+        System.out.println("@BeforeGroups");
+    }
+
     @BeforeClass
     public void setUp() {
         calculator = new Calculator();
 
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("@BeforeMethod");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("@AfterMethod");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("@AfterClass");
+    }
+
+    @AfterGroups
+    public void afterGroups() {
+        System.out.println("@AfterGroups");
     }
 }
